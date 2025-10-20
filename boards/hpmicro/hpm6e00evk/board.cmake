e1af583ecfb2debec09b83ce86807ec91153025f
Yingda WU <yingda.wu@hpmicro.com>
2025-10-20 11:44:08 +0800

    sdk_glue: add new soc and board .refs:zephyr-#5580
    - add soc hpm6280 and board hpm6200evk
    - add soc hpm6e80 and board hpm6e00evk
    
    Signed-off-by: Yingda WU <yingda.wu@hpmicro.com>

---
+++ boards/hpmicro/hpm6e00evk/board.cmake
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set(OPENOCD_CONFIG_RELATIVE ${ZEPHYR_BASE}/../sdk_env/hpm_sdk/boards/openocd)
+set(HPM_TOOLS_RELATIVE ${ZEPHYR_BASE}/../sdk_env/tools)
+get_filename_component(OPENOCD_CONFIG_ABSOLUTE ${OPENOCD_CONFIG_RELATIVE} ABSOLUTE)
+get_filename_component(HPM_TOOLS_ABSOLUTE ${HPM_TOOLS_RELATIVE} ABSOLUTE)
+set(OPENOCD_CONFIG_DIR ${OPENOCD_CONFIG_ABSOLUTE} CACHE PATH "hpmicro openocd cfg root directory")
+set(HPM_TOOLS_DIR ${HPM_TOOLS_ABSOLUTE} CACHE PATH "hpmicro win tools root directory")
+
+if(NOT CONFIG_XIP)
+    board_runner_args(openocd "--use-elf")
+endif()
+
+if(${BOARD} STREQUAL "hpm6e00evk")
+    board_runner_args(openocd "--config=${OPENOCD_CONFIG_DIR}/probes/cmsis_dap.cfg"
+                                    "--config=${OPENOCD_CONFIG_DIR}/soc/hpm6e80-single-core.cfg"
+                                "--config=${OPENOCD_CONFIG_DIR}/boards/hpm6e00vk.cfg"
+                                "--openocd-search=${OPENOCD_CONFIG_DIR}")
+    board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
+else()
+    message(FATAL_ERROR "${BOARD} is not supported now")
+endif()
+
+set(ENV_PATH $ENV{PATH})
+unset(OPENOCD CACHE)
+if("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Linux")
+    set(SEPARATOR ":")
+    string(REPLACE ${SEPARATOR} ";" ZPATH ${ENV_PATH})
+    find_program(OPENOCD openocd PATHS ${ZPATH} NO_DEFAULT_PATH REQUIRED)
+elseif("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Windows")
+    set(OPENOCD "${HPM_TOOLS_DIR}/openocd/openocd.exe" CACHE FILEPATH "" FORCE)
+    set(OPENOCD_DEFAULT_PATH ${HPM_TOOLS_DIR}/openocd/tcl)
+else()
+    message(WARNING "${CMAKE_HOST_SYSTEM_NAME} openocd is not support")
+endif()
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
---
+++ soc/hpmicro/HPM6200/soc.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2025 HPMicro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+#include <hpm_common.h>
+#include <hpm_soc.h>
+#include "hpm_clock_drv.h"
+#include "hpm_pllctl_drv.h"
+#include "hpm_pcfg_drv.h"
+#ifdef CONFIG_NOCACHE_MEMORY
+#include <zephyr/linker/linker-defs.h>
+#include "hpm_pmp_drv.h"
+#endif
+#ifdef CONFIG_XIP
+#include "hpm_bootheader.h"
+#endif
+
+#ifdef CONFIG_XIP
+__attribute__ ((section(".nor_cfg_option"))) const uint32_t option[4] = {0xfcf90002, 0x00000007, 0xE, 0x0};
+#endif
+__attribute__((weak)) void c_startup(void)
+{
+}
+
+static void soc_init_clock(void)
+{
+    uint32_t cpu0_freq = clock_get_frequency(clock_cpu0);
+    if (cpu0_freq == PLLCTL_SOC_PLL_REFCLK_FREQ) {
+        /* Configure the External OSC ramp-up time: ~9ms */
+        pllctl_xtal_set_rampup_time(HPM_PLLCTL, 32UL * 1000UL * 9U);
+
+        /* Select clock setting preset1 */
+        sysctl_clock_set_preset(HPM_SYSCTL, sysctl_preset_1);
+    }
+
+    /* Add most Clocks to group 0 */
+    /* not open uart clock in this API, uart should configure pin function before opening clock */
+    clock_add_to_group(clock_cpu0, 0);
+    clock_add_to_group(clock_mchtmr0, 0);
+    clock_add_to_group(clock_axi0, 0);
+    clock_add_to_group(clock_axi1, 0);
+    clock_add_to_group(clock_axi2, 0);
+    clock_add_to_group(clock_ahb, 0);
+    clock_add_to_group(clock_femc, 0);
+    clock_add_to_group(clock_xpi0, 0);
+    clock_add_to_group(clock_xpi1, 0);
+    clock_add_to_group(clock_gptmr0, 0);
+    clock_add_to_group(clock_gptmr1, 0);
+    clock_add_to_group(clock_gptmr2, 0);
+    clock_add_to_group(clock_gptmr3, 0);
+    clock_add_to_group(clock_gptmr4, 0);
+    clock_add_to_group(clock_gptmr5, 0);
+    clock_add_to_group(clock_gptmr6, 0);
+    clock_add_to_group(clock_gptmr7, 0);
+    clock_add_to_group(clock_i2c0, 0);
+    clock_add_to_group(clock_i2c1, 0);
+    clock_add_to_group(clock_i2c2, 0);
+    clock_add_to_group(clock_i2c3, 0);
+    clock_add_to_group(clock_spi0, 0);
+    clock_add_to_group(clock_spi1, 0);
+    clock_add_to_group(clock_spi2, 0);
+    clock_add_to_group(clock_spi3, 0);
+    clock_add_to_group(clock_can0, 0);
+    clock_add_to_group(clock_can1, 0);
+    clock_add_to_group(clock_can2, 0);
+    clock_add_to_group(clock_can3, 0);
+    clock_add_to_group(clock_display, 0);
+    clock_add_to_group(clock_sdxc0, 0);
+    clock_add_to_group(clock_sdxc1, 0);
+    clock_add_to_group(clock_camera0, 0);
+    clock_add_to_group(clock_camera1, 0);
+    clock_add_to_group(clock_ptpc, 0);
+    clock_add_to_group(clock_ref0, 0);
+    clock_add_to_group(clock_ref1, 0);
+    clock_add_to_group(clock_watchdog0, 0);
+    clock_add_to_group(clock_eth0, 0);
+    clock_add_to_group(clock_eth1, 0);
+    clock_add_to_group(clock_sdp, 0);
+    clock_add_to_group(clock_xdma, 0);
+    clock_add_to_group(clock_ram0, 0);
+    clock_add_to_group(clock_ram1, 0);
+    clock_add_to_group(clock_usb0, 0);
+    clock_add_to_group(clock_usb1, 0);
+    clock_add_to_group(clock_jpeg, 0);
+    clock_add_to_group(clock_pdma, 0);
+    clock_add_to_group(clock_kman, 0);
+    clock_add_to_group(clock_gpio, 0);
+    clock_add_to_group(clock_mbx0, 0);
+    clock_add_to_group(clock_hdma, 0);
+    clock_add_to_group(clock_rng, 0);
+    clock_add_to_group(clock_mot0, 0);
+    clock_add_to_group(clock_mot1, 0);
+    clock_add_to_group(clock_mot2, 0);
+    clock_add_to_group(clock_mot3, 0);
+    clock_add_to_group(clock_acmp, 0);
+    clock_add_to_group(clock_dao, 0);
+    clock_add_to_group(clock_synt, 0);
+    clock_add_to_group(clock_lmm0, 0);
+    clock_add_to_group(clock_lmm1, 0);
+    clock_add_to_group(clock_pdm, 0);
+
+    clock_add_to_group(clock_adc0, 0);
+    clock_add_to_group(clock_adc1, 0);
+    clock_add_to_group(clock_adc2, 0);
+    clock_add_to_group(clock_adc3, 0);
+
+    clock_add_to_group(clock_i2s0, 0);
+    clock_add_to_group(clock_i2s1, 0);
+    clock_add_to_group(clock_i2s2, 0);
+    clock_add_to_group(clock_i2s3, 0);
+    /* Connect Group0 to CPU0 */
+    clock_connect_group_to_cpu(0, 0);
+
+    /* Add the CPU1 clock to Group1 */
+    clock_add_to_group(clock_mchtmr1, 1);
+    clock_add_to_group(clock_mbx1, 1);
+    /* Connect Group1 to CPU1 */
+    clock_connect_group_to_cpu(1, 1);
+
+    /* Bump up DCDC voltage to 1200mv */
+    pcfg_dcdc_set_voltage(HPM_PCFG, 1200);
+    pcfg_dcdc_switch_to_dcm_mode(HPM_PCFG);
+
+    clock_set_source_divider(clock_cpu0, clk_src_pll0_clk0, 1);
+    clock_set_source_divider(clock_cpu1, clk_src_pll0_clk0, 1);
+
+    clock_set_source_divider(clock_ahb, clk_src_pll1_clk1, 2); /*200m hz*/
+    clock_set_source_divider(clock_mchtmr0, clk_src_osc24m, 1);
+    clock_set_source_divider(clock_mchtmr1, clk_src_osc24m, 1);
+}
+
+#ifdef CONFIG_NOCACHE_MEMORY
+static void soc_init_pmp(void)
+{
+    volatile uint32_t start_addr = (uint32_t) &_nocache_ram_start;
+    volatile uint32_t length = (uint32_t) &_nocache_ram_size;
+
+    if (length == 0) {
+        return;
+    }
+
+    /* Ensure the address and the length are power of 2 aligned */
+    assert((length & (length - 1U)) == 0U);
+    assert((start_addr & (length - 1U)) == 0U);
+
+    pmp_entry_t pmp_entry[3] = { 0 };
+    pmp_entry[0].pmp_addr = PMP_NAPOT_ADDR(0x0000000, 0x80000000);
+    pmp_entry[0].pmp_cfg.val = PMP_CFG(READ_EN, WRITE_EN, EXECUTE_EN, ADDR_MATCH_NAPOT, REG_UNLOCK);
+
+
+    pmp_entry[1].pmp_addr = PMP_NAPOT_ADDR(0x80000000, 0x80000000);
+    pmp_entry[1].pmp_cfg.val = PMP_CFG(READ_EN, WRITE_EN, EXECUTE_EN, ADDR_MATCH_NAPOT, REG_UNLOCK);
+
+    pmp_entry[2].pmp_addr = PMP_NAPOT_ADDR(start_addr, length);
+    pmp_entry[2].pmp_cfg.val = PMP_CFG(READ_EN, WRITE_EN, EXECUTE_EN, ADDR_MATCH_NAPOT, REG_UNLOCK);
+    pmp_entry[2].pma_addr = PMA_NAPOT_ADDR(start_addr, length);
+    pmp_entry[2].pma_cfg.val = PMA_CFG(ADDR_MATCH_NAPOT, MEM_TYPE_MEM_NON_CACHE_BUF, AMO_EN);
+    pmp_config(&pmp_entry[0], ARRAY_SIZE(pmp_entry));
+}
+#endif
+
+static int hpmicro_soc_init(void)
+{
+	uint32_t key;
+
+	key = irq_lock();
+	soc_init_clock();
+#ifdef CONFIG_NOCACHE_MEMORY
+	soc_init_pmp();
+#endif
+	irq_unlock(key);
+
+	return 0;
+}
+
+SYS_INIT(hpmicro_soc_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
